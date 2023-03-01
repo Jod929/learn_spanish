049e3fc754d0dd0ec2d951ff48533c5bf7cffe25
Josh Krebs <joddykrebs4@gmail.com>
2023-02-28 16:49:52 -0800

    change to env

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 const path = require('path');
 const port = process.env.PORT || 8080;
 const client = path.resolve(__dirname, '../client/dist');
