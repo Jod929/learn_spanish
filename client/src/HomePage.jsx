2fddb6a93c83682a6153e18e6e839982d3ae2203
Josh Krebs <joddykrebs4@gmail.com>
2023-03-09 14:49:27 -0800

    installed npx

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -29,7 +29,7 @@ const HomePage = () => {
   return (
     <div className="homepage-container">
       <div className="homepage-header">
-        <h1>one more</h1>
+        <h1>installed npmx</h1>
         <h2>Please checkout all conjugations and use the flashcards to learn!</h2>
         <button onClick={() => {setPage('Flash')}}>Conjugation Flash Cards</button>
         <button onClick={() => {setPage('Conj')}}>See All Conjugations</button>
