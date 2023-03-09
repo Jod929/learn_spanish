aa9e1871bd0830d369ff62cef14ca2deee5e0c4c
Josh Krebs <joddykrebs4@gmail.com>
2023-03-09 15:37:37 -0800

    try with cd

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -8,6 +8,8 @@ echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/learn_spanish/deploy.log
 
 sudo pm2 restart learn_spanish >> /home/ec2-user/learn_spanish/deploy.log
 
+cd /home/ec2-user/learn_spanish
+
 echo 'running build' >> /home/ec2-user/learn_spanish/deploy.log
 
 npm run build >> /home/ec2-user/learn_spanish/deploy.log
