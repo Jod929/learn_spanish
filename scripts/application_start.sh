e3bc6509179787c0fd64a705e2416646682e6d34
Josh Krebs <joddykrebs4@gmail.com>
2023-03-09 16:00:22 -0800

    added symbolic link

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,12 +3,12 @@ sudo chmod -R 777 /home/ec2-user/learn_spanish
 
 echo 'run application_start.sh: ' >> /home/ec2-user/learn_spanish/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/learn_spanish/deploy.log
-
-# sudo pm2 restart learn_spanish >> /home/ec2-user/learn_spanish/deploy.log
-
-cd /home/ec2-user/learn_spanish
-
 echo 'running build' >> /home/ec2-user/learn_spanish/deploy.log
 
 npm run build >> /home/ec2-user/learn_spanish/deploy.log
+
+echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/learn_spanish/deploy.log
+
+sudo pm2 restart learn_spanish >> /home/ec2-user/learn_spanish/deploy.log
+
+cd /home/ec2-user/learn_spanish
